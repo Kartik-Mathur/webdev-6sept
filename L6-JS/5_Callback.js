690712f72863ce6ce3e38845a365df87c0c2af76
Kartik <kartik.mathur10@gmail.com>
2026-09-26 15:43:16 +0530

    Callback

---
+++ L6-JS/5_Callback.js
@@ -0,0 +1,21 @@
+// Jab ek kaam poora ho jaaye uske baad next kaam karna is callback
+// Jis function ko hum ek kaam complete ho jaane ke baad call karte hai
+// that is called as Callback Function.....
+
+// Async functions ko (jo java script ka thread block nhi krte)
+// Unhe sequential banane mei help karta hai.....
+
+function getMaggi(cb) {
+  setTimeout(function () {
+    console.log("Maggi aa gai");
+    cb()
+  }, 3000);
+}
+
+function makeMaggi() {
+  setTimeout(function () {
+    console.log("Maggi bann gai");
+  }, 2000);
+}
+
+getMaggi(makeMaggi);
---
+++ L6-JS/6_Callback.js
@@ -0,0 +1,15 @@
+function download(url) {
+  // give async delay of 2 second
+}
+
+function compress(songName) {
+  // power.zip aage jaega
+  // give async delay of 2 second
+}
+
+function upload(compressedSong) {
+  // upload ke baad newurl would be https://newsongs.mp4/power.zip
+  // give async delay of 2 second
+}
+
+download("http://songs.com/power.mp4");
